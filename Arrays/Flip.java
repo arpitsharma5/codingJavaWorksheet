8e16869a828dc6490e0ea57c32b4921aeb224c57
SG0303266 <arpit.sharma@sabre.com>
2024-09-17 18:52:10 +0530

    first missing integer

--- Arrays/Flip.java
+++ Arrays/Flip.java
@@ -1,12 +1,17 @@
 package com.practiseJava.Arrays;
 
 import java.util.ArrayList;
+/*
+* You are given a binary string A(i.e. with characters 0 and 1) consisting of characters A1, A2, ..., AN. In a single operation, you can choose two indices L and R such that 1 ≤ L ≤ R ≤ N and flip the characters AL, AL+1, ..., AR. By flipping, we mean change character 0 to 1 and vice-versa.
 
+Your aim is to perform ATMOST one operation such that in final string number of 1s is maximised.
+
+If you don't want to perform the operation, return an empty array. Else, return an array consisting of two elements denoting L and R. If there are multiple solutions, return the lexicographically smallest pair of L and R.*/
 public class Flip {
   public static void main(String[] args) {
     Flip flip = new Flip();
-//    System.out.println(flip.flip("1101010001"));
-    System.out.println(flip.flip("01100"));
+    System.out.println(flip.flip("1101010001")); // 3,9
+//    System.out.println(flip.flip("01100"));
   }
 
   public ArrayList<Integer> flip(String A) {
