1834a6aa945fee42511e4ff3ada3aeabcdc37556
SG0303266 <arpit.sharma@sabre.com>
2024-10-09 19:53:33 +0530

    max distance practice 10/9

--- Arrays/MaxDistance.java
+++ Arrays/MaxDistance.java
@@ -2,6 +2,7 @@ package com.practiseJava.Arrays;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,63 +12,37 @@ import java.util.Map;
 * */
 public class MaxDistance {
   public static void main(String[] args) {
-    ArrayList<Integer> inp = new ArrayList<>(
-        List.of(3, 5, 4, 2));
+
+    int[] inp = new int[]{9, 2, 3, 4, 5, 6, 7, 8, 18, 0};
     System.out.println(maxDistance(inp));
   }
-  public static int maxDistance(List<Integer> list) {
-    int size = list.size();
-
-    List<Helper> helpers = new ArrayList<>(size);
-    Map<Integer, Integer> mp = new HashMap<>(size);
-    // Add each element into an helper array
-    for (int i = 0; i < size; i++) {
-      helpers.add(new Helper(list.get(i), i));
-    }
-    // Sort the helper array
-    Collections.sort(helpers);
-
-    int[] rightMax = new int[size];
-    int maxValue = Integer.MIN_VALUE;
-
-    // Store the max value from the right to the left
-    for (int i = size - 1; i >= 0; i--) {
-      maxValue = Math.max(maxValue, helpers.get(i).index);
-      rightMax[i] = maxValue;
+  public static int maxDistance(int[] list) {
+    int n = list.length;
+    int maxDiff;
+    int rmax[] = new int[n];
+    int lmin[] = new int[n];
+    lmin[0] = list[0];
+    for(int i = 1; i < n ; i++) {
+      lmin[i] = Math.min(list[i], lmin[i-1]);
     }
 
-    int maxDiff = Integer.MIN_VALUE;
-
-    // Iterate over helper and check the diff value
-    for (int i = 0; i < size; i++) {
-      maxDiff = Math.max(maxDiff, rightMax[i] - helpers.get(i).index);
+    rmax[n-1] = list[n-1];
+    for(int j = n-2; j >= 0 ; j--) {
+      rmax[j] = Math.max(list[j], rmax[j+1]);
     }
 
+    int i=0;
+    int j=0;
+    maxDiff = Integer.MIN_VALUE;
+    while (i< n && j < n) {
+      if(lmin[i] <= rmax[j]) {
+        maxDiff = Math.max(maxDiff, j-i);
+        j++;
+      } else {
+        i++;
+      }
+    }
     return maxDiff;
-
-  }
   }
 
-
-class Helper implements Comparable<Helper> {
-  public int value;
-  public int index;
-
-  public Helper(int value, int index) {
-    this.value = value;
-    this.index = index;
-  }
-
-  @Override
-  public int compareTo(Helper helper) {
-    // Comparison on the value
-
-    if (value < helper.value) {
-      return -1;
-    } else if (value == helper.value) {
-      return 0;
-    } else {
-      return 1;
-    }
-  }
 }
