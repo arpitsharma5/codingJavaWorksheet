fd010176240ba9fc9c8896952db048842be77557
SG0303266 <arpit.sharma@sabre.com>
2024-10-15 20:01:57 +0530

    max sum triplet practice 15/10

--- Arrays/MaximumSumTriplet.java
+++ Arrays/MaximumSumTriplet.java
@@ -11,23 +11,22 @@ public class MaximumSumTriplet {
   public static void main(String[] args) {
     MaximumSumTriplet largest = new MaximumSumTriplet();
     ArrayList<Integer> inp = new ArrayList<>(
-        List.of(2, 5, 3, 1, 4, 2 ));
+        List.of(22649, 27447, 23806, 15891, 6730, 24371, 15351, 15007, 31102, 24394, 3549, 19630, 12624, 24085 ));
     System.out.println(largest.maximumSumTriplet(inp));
   }
   public int maximumSumTriplet(List<Integer> A) {
     int n = A.size();
-    int[] maxSumSuffix = new int[n+1];
-    maxSumSuffix[n] = 0;
-    for(int i = n -1; i>=0;i-- ) {
-      maxSumSuffix[i] = Math.max(maxSumSuffix[i+1], A.get(i));
+    int[] maxRightSuffix = new int[n];
+    maxRightSuffix[n-1] = A.get(n-1);
+    for(int i = n-2; i >= 0; i--) {
+      maxRightSuffix[i] = Math.max(maxRightSuffix[i+1], A.get(i));
     }
-    int ans = 0;
     TreeSet<Integer> lowValue = new TreeSet<>();
     lowValue.add(Integer.MIN_VALUE);
-
+    int ans=0;
     for(int i = 0; i< n-1; i++) {
-      if(maxSumSuffix[i+1] > A.get(i)) {
-        ans = Math.max(ans, lowValue.lower(A.get(i)) + A.get(i) + maxSumSuffix[i+1]);
+      if(maxRightSuffix[i] > A.get(i)) {
+        ans = Math.max(ans, lowValue.lower(A.get(i)) + A.get(i) + maxRightSuffix[i]);
         lowValue.add(A.get(i));
       }
     }
